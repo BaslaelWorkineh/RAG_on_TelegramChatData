48a90bbb20a73e6d5a7c953951d53566e056e3a8
BaslaelWorkineh <baslaworku@gmail.com>
2024-07-19 10:02:31 +0300

    database deleted

--- setup.sh
+++ setup.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-# Create and activate virtual environment
 python3 -m venv rag_env
 source rag_env/bin/activate
 
-# Upgrade pip
 pip install --upgrade pip
 
-# Install dependencies
 pip install -r requirements.txt
 
-# Download SpaCy language model
 python -m spacy download en_core_web_sm
